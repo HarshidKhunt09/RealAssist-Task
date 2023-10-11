1fbaee7cacc46f0264b9f64fcb78fc441314a4b9
harshidkhunt <harshidkhunt09@gmail.com>
2023-10-11 08:59:15 +0530

    Changes

--- server/server.js
+++ server/server.js
@@ -10,7 +10,19 @@ app.use(cors());
 
 app.get('/pdf', async (req, res) => {
   try {
-    const browser = await puppeteer.launch({ headless: 'new' });
+    const browser = await puppeteer.launch({
+      args: [
+        '--disable-setuid-sandbox',
+        '--no-sandbox',
+        '--single-process',
+        '--no-zygote',
+      ],
+      executablePath:
+        process.env.NODE_ENV === 'production'
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+      headless: 'new',
+    });
     const page = await browser.newPage();
 
     await page.goto(process.env.WEB_PAGE_TO_PDF_URL);
